a94f052e2bddb71b919b21896fc51aca07753b1d
aaearon <aaearon@gmail.com>
2014-11-21 11:46:46 +0100

    removed getters and setters from model classes but keeping the classes as a reference

--- app/src/main/java/com/iosharp/android/ssplayer/model/Channel.java
+++ app/src/main/java/com/iosharp/android/ssplayer/model/Channel.java
@@ -7,37 +7,4 @@ public class Channel {
 
     public Channel() {
     }
-
-    public int getId() {
-        return mId;
-    }
-
-    public void setId(int id) {
-        mId = id;
-    }
-
-    public String getName() {
-        return mName;
-    }
-
-    public void setName(String name) {
-        mName = name;
-    }
-
-    public String getIcon() {
-        return mIcon;
-    }
-
-    public void setIcon(String icon) {
-        mIcon = icon;
-    }
-
-    @Override
-    public String toString() {
-        return "Channel{" +
-                "mId=" + mId +
-                ", mName='" + mName + '\'' +
-                ", mIcon='" + mIcon + '\'' +
-                '}';
-    }
-}
+ }
--- app/src/main/java/com/iosharp/android/ssplayer/model/Event.java
+++ app/src/main/java/com/iosharp/android/ssplayer/model/Event.java
@@ -11,107 +11,10 @@ public class Event {
     private long mRuntime;
     private int mChannel;
     private String mLanguage;
-    private String mCategory;
+    private String mCategory; // Not retrieved
     private String mQuality;
 
     public Event() {
     }
 
-    public int getId() {
-        return mId;
     }
-
-    public void setId(int id) {
-        mId = id;
-    }
-
-    public String getNetwork() {
-        return mNetwork;
-    }
-
-    public void setNetwork(String network) {
-        mNetwork = network;
-    }
-
-    public String getName() {
-        return mName;
-    }
-
-    public void setName(String name) {
-        mName = name;
-    }
-
-    public long getRuntime() {
-        return mRuntime;
-    }
-
-    public void setRuntime(long runtime) {
-        mRuntime = runtime;
-    }
-
-    public String getLanguage() {
-        return mLanguage;
-    }
-
-    public void setLanguage(String language) {
-        mLanguage = language;
-    }
-
-    public String getCategory() {
-        return mCategory;
-    }
-
-    public void setCategory(String category) {
-        mCategory = category;
-    }
-
-    public String getQuality() {
-        return mQuality;
-    }
-
-    public void setQuality(String quality) {
-        mQuality = quality;
-    }
-
-    public int getChannel() {
-        return mChannel;
-    }
-
-    public void setChannel(int channel) {
-        mChannel = channel;
-    }
-
-    public long getStartDate() {
-        return mStartDate;
-    }
-
-    public void setStartDate(long startDate) {
-        mStartDate = startDate;
-    }
-
-    public long getEndDate() {
-        return mEndDate;
-    }
-
-    public void setEndDate(long endDate) {
-        mEndDate = endDate;
-    }
-
-    public String getDescription() {
-        return mDescription;
-    }
-
-    public void setDescription(String description) {
-        mDescription = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                "mId=" + mId +
-                ", mName='" + mName + '\'' +
-                ", mStartDate=" + mStartDate +
-                ", mChannel=" + mChannel +
-                '}';
-    }
-}
